744b248f840168ae7f88336289f2e8bd3b88653f
Ramya <ramyareddym023@gmail.com>
2024-12-14 10:38:44 +0530

    Add files via upload

---
+++ Multiple_Inheritance.java
@@ -0,0 +1,28 @@
+import java.util.*;
+interface A{
+public void r();
+} 
+interface B
+{
+    public void r1();
+}
+class C implements A,B{
+public void r()
+{
+    System.out.println("Hello");
+}
+public void r1()
+{
+    System.out.println("Hii");
+}
+} 
+public class Multiple_Inheritance
+{
+    public static void main(String args[])
+    {
+        C h1=new C();
+        h1.r();
+        h1.r1();
+
+    }
+}
