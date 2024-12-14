78800d2b4843244578dfb618165f55ebcf9426db
Ramya <ramyareddym023@gmail.com>
2024-12-14 14:46:35 +0530

    Throw.java

---
+++ Throw.java
@@ -0,0 +1,14 @@
+  public class Throw
+  {
+    public static void main(String args[])
+    {
+      //try-catch
+      try{
+        throw new Exception("Error");
+      }
+      catch(Exception e)
+      {
+        System.out.println(e.getMessage());
+      }
+    }
+  }
