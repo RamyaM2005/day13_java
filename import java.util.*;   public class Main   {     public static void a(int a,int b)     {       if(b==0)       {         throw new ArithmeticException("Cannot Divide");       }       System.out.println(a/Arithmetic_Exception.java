2cc03a4a7a7ffb2f6e6750298f61c81ce730f51e
Ramya <ramyareddym023@gmail.com>
2024-12-14 15:04:28 +0530

    Arithmetic_Exception.java

---
+++ import java.util.*;   public class Main   {     public static void a(int a,int b)     {       if(b==0)       {         throw new ArithmeticException("Cannot Divide");       }       System.out.println(a/Arithmetic_Exception.java
@@ -0,0 +1,24 @@
+ import java.util.*;
+  public class Arithmetic_Exception
+  {
+    public static void a(int a,int b)
+    {
+      if(b==0)
+      {
+        throw new ArithmeticException("Cannot Divide");
+      }
+      System.out.println(a/b);
+    }
+      public static void main(String args[])
+      {
+        try
+        {
+          a(10,0);
+        }
+        catch(ArithmeticException e)
+        {
+        System.out.println(e.getMessage());
+        }
+      }
+    }
+  
